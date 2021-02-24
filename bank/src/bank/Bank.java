5a7bb3d704b54ae5085c4ebd1fd497d49bf5de3e
kliu <kliu54@binghamton.edu>
2021-02-24 03:55:00 -0500

    commit

---
+++ bank/src/bank/Bank.java
@@ -0,0 +1,29 @@
+package bank;
+
+public class Bank {
+	int accountNumber = 123456789;
+	double balance = 500.0;
+	String name = "Richard";
+	String email = "abc@gmail.com";
+	String number = "8888888888";
+	double newBalance;
+	
+	public void deposite(int num) {
+		newBalance = balance + num;
+		System.out.println(newBalance);
+	}
+	public void withdraw(int num) {
+		if (balance - num > 0) {
+			newBalance = balance - num;
+		}
+		System.out.println(newBalance);
+	}
+	public void getCustomerInfo() {
+			System.out.println("Customer Name is "+ name);
+			System.out.println("Account Number is "+accountNumber);
+			System.out.println("Balnce is " + balance);
+			System.out.println("Email is "+ email);
+			System.out.println("Phone Nmber is "+ number);
+			
+	}
+}
---
+++ bank/src/bank/Test.java
@@ -0,0 +1,11 @@
+package bank;
+
+public class Test {
+	public static void main(String args) {
+		Bank bank = new Bank();
+		bank.deposite(500);
+		bank.withdraw(300);
+		bank.getCustomerInfo();
+	} 
+	
+}
